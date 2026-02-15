820406b3f2ab8a3f17fd5920d9b42256ec7c60a0
QUOJO DAWSON <142614595+QUOJO-DAWSON@users.noreply.github.com>
2026-02-15 10:06:43 -0500

    feat: add enhanced CloudWatch alarms for errors, latency, and throttling

---
+++ aws-free-tier/terraform/cloudwatch-alarms.tf
@@ -0,0 +1,57 @@
+﻿# Enhanced CloudWatch Alarms
+
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  alarm_name          = "${var.project_name}-lambda-errors"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "300"
+  statistic           = "Sum"
+  threshold           = "5"
+  alarm_description   = "Alert when Lambda function errors exceed threshold"
+  
+  dimensions = {
+    FunctionName = aws_lambda_function.visitor_counter.function_name
+  }
+
+  tags = {
+    Name = "Lambda Error Alarm"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "api_high_latency" {
+  alarm_name          = "${var.project_name}-api-high-latency"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "Latency"
+  namespace           = "AWS/ApiGateway"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "1000"
+  alarm_description   = "Alert when API latency exceeds 1 second"
+
+  tags = {
+    Name = "API Latency Alarm"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda_throttles" {
+  alarm_name          = "${var.project_name}-lambda-throttles"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Throttles"
+  namespace           = "AWS/Lambda"
+  period              = "300"
+  statistic           = "Sum"
+  threshold           = "10"
+  alarm_description   = "Alert when Lambda function is throttled"
+  
+  dimensions = {
+    FunctionName = aws_lambda_function.visitor_counter.function_name
+  }
+
+  tags = {
+    Name = "Lambda Throttle Alarm"
+  }
+}
